9301aa87c81e4f369b7473ba48464d43cf6522bb
ORLANDO JAVIER MEDINA MELENDEZ <orlando.medinamlnd@uanl.edu.mx>
2021-02-25 13:15:51 -0600

    Codigo base

---
+++ Models/VPregunta1.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace Tarea1.Models
+{
+    public partial class VPregunta1
+    {
+        public string Idcategoria { get; set; }
+        public int Categoria { get; set; }
+        public int? Nproductos { get; set; }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Tarea1.Models;
+using System.Linq;
+
+namespace Tarea1
+{
+    class Program
+    {
+        public static void exercise()
+        {
+            NorthwindContext dataContext = new NorthwindContext();
+
+            var employeeQuery = dataContext.Employees.Select(s => s).AsQueryable();
+
+            var output = employeeQuery.ToList();
+        }
+
+        static void Main(string[] args)
+        {
+            exercise();
+        }
+    }
+}
